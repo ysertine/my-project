072093bfde9186fcab0ea7923a238917cef5a9aa
dengjinbo <335196272@qq.com>
2018-11-08 17:25:43 +0800

    项目目录结构调整；

--- src/main/java/com/my/project/CoreApplication.java
+++ src/main/java/com/my/project/CoreApplication.java
@@ -1,4 +1,4 @@
-package com.my.project.template;
+package com.my.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/my/project/system/controller/HelloWorldController.java
+++ src/main/java/com/my/project/system/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.my.project.template.controller;
+package com.my.project.system.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
